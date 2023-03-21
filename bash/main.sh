062c8d60649c991c48a14f4b72c715a991dc4f7e
Andrej Podhradsky <apodhrad@redhat.com>
2023-03-21 15:16:28 +0100

    Update bash/main.sh with more details
    Signed-off-by: Andrej Podhradsky <apodhrad@redhat.com>

--- bash/main.sh
+++ bash/main.sh
@@ -1,6 +1,8 @@
-main() {
-  echo "main code"
-}
+# ${0} is the 1st argument, i.e. script name
+# ${BASH_SOURCE} is the script name which is executed
+# ${BASH_SOURCE[0]} is the same as ${BASH_SOURCE}
+# ${BASH_SOURCE[1]} is the script name which sourced ${BASH_SOURCE[0]}
+# ${BASH_SOURCE[2]} is the script name which sourced ${BASH_SOURCE[1]}
 
 # to avoid execution when sourced
 [[ "${BASH_SOURCE[0]}" == "${0}" ]] && main "$@"
