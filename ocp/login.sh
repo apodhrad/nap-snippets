53020898b061d459d5498c30b7993fc1518a7b9f
Andrej Podhradsky <apodhrad@redhat.com>
2023-04-04 14:55:17 +0200

    Update ocp/login.sh
    Signed-off-by: Andrej Podhradsky <apodhrad@redhat.com>

--- ocp/login.sh
+++ ocp/login.sh
@@ -1,1 +1,13 @@
-oc login -u kubeadmin -p $PASS $APISERVER --insecure-skip-tls-verify=true
+# with oc login
+oc login --token "${TOKEN}" --insecure-skip-tls-verify=true
+oc login -u kubeadmin -p "${PASS}" "${APISERVER}" --insecure-skip-tls-verify=true
+
+# with KUBECONFIG
+KUBECONFIG=/path/to/kubeconfig oc get pods
+# or
+export KUBECONFIG=/path/to/kubeconfig
+oc get pods
+
+# with --context <CONTEXT_NAME>
+# cat ~/.kube/config | yq -r '.contexts[].name'
+oc --context "${CONTEXT_NAME}" get pods
